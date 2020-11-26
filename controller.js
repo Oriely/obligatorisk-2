c5f1f3bfa37a3132c7c0dab4663b88db3b70945c
Oriely <orielygaming@protonmail.com>
2020-11-26 14:55:16 +0100

    Update controller.js

--- controller.js
+++ controller.js
@@ -7,9 +7,9 @@ function rmBar() {
 } 
 
 function editBar() {
-    return (chosenBar ? (inputValue <= 10 ? (numbers[chosenBar] = inputValue) : errorMsg = 'Velg et tall mellom 1 og 10') : errorMsg = 'Vennligst velg en bar') && show();
+    return (chosenBar ? (inputValue <= 10 && inputValue >= 1 ? (numbers[chosenBar] = inputValue) : errorMsg = 'Velg et tall mellom 1 og 10') : errorMsg = 'Vennligst velg en bar') && show();
 } 
 
 function addBar(){
-    return (inputValue !== undefined ? (inputValue <= 10 ? numbers.push(inputValue) : errorMsg = 'Velg et tall mellom 1 og 10') : errorMsg = 'Vennligst velg et tall') && show();
+    return (inputValue !== undefined ? (inputValue <= 10 && inputValue >= 1 ? numbers.push(inputValue) : errorMsg = 'Velg et tall mellom 1 og 10') : errorMsg = 'Vennligst velg et tall') && show();
 }
